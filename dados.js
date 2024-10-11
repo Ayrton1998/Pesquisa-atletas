49d3043b16cc378b627676855e51ee7ea6087b04
Ayrton1998 <senninha764@gmail.com>
2024-10-11 18:53:58 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo:"Gabigol",
+        descricao:"Gabigol jogador do Flamengo",
+        link:"https://pt.wikipedia.org/wiki/Gabigol",
+        tags:"gabigol babriel barbosa flamengo"
+    },
+    {
+        titulo:"Ayrton Senna",
+        descricao:"Ayrton Senna piloto de formula 1",
+        link:"https://pt.wikipedia.org/wiki/Ayrton_Senna",
+        tags:"Ayrton Senna formula 1 corrida"
+    },
+
+    {
+        titulo:"Neymar",
+        descricao:"Neymar ídolo Brasileiro",
+        link:"https://pt.wikipedia.org/wiki/Neymar",
+        tags:"queda dribles brasileiro dança"
+    }
+
+];
